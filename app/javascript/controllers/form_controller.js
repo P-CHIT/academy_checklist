fe58e54d7eddc0331ca59d1b3283c16bc9601267
Phuchit <phuchit.u@gmail.com>
2025-08-20 18:28:19 +0700

    fix: reset form after create task

---
+++ app/javascript/controllers/form_controller.js
@@ -0,0 +1,12 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="form"
+export default class extends Controller {
+  static targets = [ "form", "name" ]
+
+  submit(event){
+    event.preventDefault();
+    this.formTarget.requestSubmit();
+    this.nameTarget.value = "";
+  }
+}
