ebcc09e46927a458270b82251df109b61e96581a
Phuchit <phuchit.u@gmail.com>
2025-08-20 10:23:18 +0700

    feat: add delete function

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -53,7 +53,7 @@ class TasksController < ApplicationController
     @task.destroy!
 
     respond_to do |format|
-      format.html { redirect_to tasks_path, notice: "Task was successfully destroyed.", status: :see_other }
+      format.html { redirect_to root_path, alert: "Task was successfully destroyed.", status: :see_other }
       format.json { head :no_content }
     end
   end
